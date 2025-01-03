d0541d1ad43aeac95771916dd3ee9fd781f59ceb
G.Hazar Polat <gppolat@icloud.com>
2025-01-03 22:25:28 +0300

    v1

---
+++ src/Runner.java
@@ -0,0 +1,46 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //version 1
+        System.out.println("değişiklik 1");//ws
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
